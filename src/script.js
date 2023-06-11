75e88c3c426012ffe5f81cc80ebfa3c6fc912f19
Eduardo Torres Cruz <etorre35@ucsc.edu>
2023-06-10 22:37:44 -0700

    added interactivity to outro

--- src/script.js
+++ src/script.js
@@ -177,33 +177,76 @@ class Intro extends Phaser.Scene {
 
 class Outro extends Phaser.Scene {
     constructor() {
-        super('outro')
-    }
-
-    preload() {
-        // this.load.path = '/assets/' // local
-        this.load.path = '/Memoria/assets/' // github
-        this.load.audio('page', 'sounds/page.mp3')
+        super("outro");
     }
 
     create() {
-        this.cameras.main.fadeIn(1000);
-        this.add.text(game.config.width/2, game.config.height/2, "After collecting a handful of memorabilia, the past that you\nhad been shutting out begins to come back to you. You remember\nhow awful your father was over the fact that he didn't have a\nson. How your mother went crazy after the loss of your\nbrother. You're not quite sure what happened in there, or how\neverything looked the way it did twenty years ago, but you\nwere sure of one thing. It wasn't your fault.")
-            .setFontSize(50)
-            .setOrigin(0.5)
+        this.graphics = this.add.graphics();
 
-        this.page = this.sound.add('page').setVolume(0.25)
-        this.input.on('pointerdown', () => {
-            this.scene.start('credits');
+        // create background
+        this.graphics.fillStyle(0x000000, 1);
+        let bg = this.graphics.fillRect(0, 0, this.game.config.width, this.game.config.height);
+
+        // Create main text
+        this.label = this.add.text(50, 100, '', {fontSize: 50, color: '#FFFFFF'}).setWordWrapWidth(this.game.config.width);
+        this.typewriteText("After collecting a handful of memorabilia, the past that you\nhad been shutting out begins to come back to you. You remember\nhow awful your father was over the fact that he didn't have a\nson. How your mother went crazy after the loss of your\nbrother. You're not quite sure what happened in there, or how\neverything looked the way it did twenty years ago, but you\nwere sure of one thing. It wasn't your fault.");
+
+        // Create text link which flashes
+        let end_text = this.add.text(this.game.config.width * 0.48, this.game.config.height * 0.7, 'Find Peace', {fontSize: 30, color: '#FFFFFF'})
+            .setInteractive({useHandCursor: true})
+            .setFontSize(25)
+            .on('pointerover', () => {
+                end_text.alpha = 1
+                flash_text_tween.pause();
+            })
+            .on('pointerout', () => {
+                flash_text_tween.resume();
+            })
+            .on('pointerdown', () => {
+                this.scene.start('credits');
         });
-        // this.time.addEvent({
-        //     delay: 25000,
-        //     loop: false,
-        //     callback: () => {
-        //         this.page.play()
-        //         this.scene.start('credits');
-        //     }
-        // });
+
+        // Tween for text to flash
+        let flash_text_tween = this.tweens.add({
+            targets: end_text,
+            alpha: 1,
+            duration: 2000,
+            yoyo: true,
+            repeat: -1,
+        });
+
+        // Initially have interactive text invisible and tween paused
+        flash_text_tween.pause()
+        end_text.alpha = 0;
+
+        this.time.delayedCall(35000, () => {
+            flash_text_tween.resume();
+        })
+
+        // Create stars when cursor moves
+        this.input.on('pointermove' , () => {
+            const x = Phaser.Math.Between(0, this.game.config.width);
+            const y = Phaser.Math.Between(0, this.game.config.height);
+
+            // create star
+            this.graphics.fillStyle(0xFFFFFF, 1);
+            let star = this.graphics.fillCircle(x, y, 2);
+        })
+    }
+
+    // Tween animation for typing effect
+    // code from "https://blog.ourcade.co/posts/2020/phaser-3-typewriter-text-effect-bitmap/"
+    typewriteText(text){
+	    const length = text.length
+	    let i = 0
+	    this.time.addEvent({
+		    callback: () => {
+			    this.label.text += text[i]
+			    ++i
+		    },
+		    repeat: length - 1,
+		    delay: 80
+	    })
     }
 }
 
