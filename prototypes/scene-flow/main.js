81b958036f4e7a6af01b039110a48897b4d34d46
JuiceBox7 <juiceinbox7@gmail.com>
2023-06-10 22:25:15 -0700

    added scene flow prototype

---
+++ prototypes/scene-flow/main.js
@@ -0,0 +1,140 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super('title')
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0x034356)
+        this.add.text(400, 300, 'This is the title\nthe menu is loading')
+            .setFontSize(30)
+            .setOrigin(0.5)
+        
+        this.time.addEvent({
+            delay: 2000,
+            loop: false,
+            callback: () => {
+                this.scene.start('menu')
+            }
+        })
+    }
+}
+
+class Menu extends Phaser.Scene {
+    constructor() {
+        super('menu')
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0x034356)
+        this.add.text(400, 150, 'This is the Menu')
+            .setFontSize(30)
+            .setOrigin(0.5)
+        
+        let button = this.add.text(400, 300, 'click here to progress to gameplay scene')
+            .setFontSize(20)
+            .setOrigin(0.5)
+            .setInteractive({useHandCursor: true})
+            .on('pointerover', () => button.setFontSize(30))
+            .on('pointerout', () => button.setFontSize(20))
+            .on('pointerdown', () => {
+                this.cameras.main.fadeOut(1000)
+                this.time.addEvent({
+                    delay: 2000,
+                    loop: false,
+                    callback: () => {
+                        this.scene.start('gameplay')
+                    }
+                })
+            })
+    }
+}
+
+class Gameplay extends Phaser.Scene {
+    constructor() {
+        super('gameplay')
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0x034356)
+        this.cameras.main.fadeIn(1000);
+        this.add.text(400, 100, 'This is the living room scene')
+            .setFontSize(30)
+            .setOrigin(0.5)
+        
+        let master = this.add.text(600, 300, 'click here to progress to master bedroom')
+            .setFontSize(20)
+            .setOrigin(0.5)
+            .setInteractive({useHandCursor: true})
+            .on('pointerover', () => master.setFontSize(30))
+            .on('pointerout', () => master.setFontSize(20))
+            .on('pointerdown', () => {
+                this.cameras.main.fadeOut(1000)
+                this.time.addEvent({
+                    delay: 2000,
+                    loop: false,
+                    callback: () => {
+                        this.scene.start('master')
+                    }
+                })
+            })
+    }
+}
+
+class Master extends Phaser.Scene {
+    constructor() {
+        super('master')
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0x034356)
+        this.cameras.main.fadeIn(1000);
+        let master = this.add.text(400, 300, 'click here to go back')
+            .setFontSize(20)
+            .setOrigin(0.5)
+            .setInteractive({useHandCursor: true})
+            .on('pointerover', () => master.setFontSize(30))
+            .on('pointerout', () => master.setFontSize(20))
+            .on('pointerdown', () => this.scene.start('gameplay'))
+
+        let end = this.add.text(400, 500, 'click here to go to credits ')
+            .setFontSize(20)
+            .setOrigin(0.5)
+            .setInteractive({useHandCursor: true})
+            .on('pointerover', () => end.setFontSize(30))
+            .on('pointerout', () => end.setFontSize(20))
+            .on('pointerdown', () => {
+                this.cameras.main.fadeOut(1000)
+                this.time.addEvent({
+                    delay: 2000,
+                    loop: false,
+                    callback: () => {
+                        this.scene.start('credits')
+                    }
+                })
+            })
+    }
+}
+
+class Credits extends Phaser.Scene {
+    constructor() {
+        super('credits')
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0x034356)
+        this.cameras.main.fadeIn(1000);
+        this.add.text(game.config.width/2, game.config.height/2, 'Thank You For Playing!\n\n       Credits:\n     Julian Lara\n Elyzza Joyce Marquez\n      Xing Zhong\n Eduardo Torres Cruz')
+            .setFontSize(30)
+            .setOrigin(0.5)
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 800,
+    backgroundColor: '#000000',
+    scene: [Title, Menu, Gameplay, Master, Credits],
+};
+
+let game = new Phaser.Game(config);
